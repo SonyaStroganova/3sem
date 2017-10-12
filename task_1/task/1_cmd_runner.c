1529697f9dcb2344d9c19f3d1aa338c4c1e07b35
SonyaStroganova <32203621+SonyaStroganova@users.noreply.github.com>
2017-10-13 00:09:50 +0300

    Update 1_cmd_runner.c
    я записала строку в двумерный массив mas  и передаю его в execvp, но функция все равно принимает только первое слово почему-то

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -22,23 +22,50 @@ int main(int atgc, char *argv[], char *envp[])
 {	
 	pid_t pid;
 	int status;
+	char *arg[100];   
+	char mas[100][100] = {""};
+	char str[1000];
+	int len, pos, i=0, j = 0;
+	
+
 	while(1) 
 	{
-		char str[255] = "";
 		gets(str);
+		len = strlen(str);
 		
+		for(i=0; i<len; i++)
+		{	
+
+			if (str[i] == ' '|| str[i] == '\0' || str[i] == '\n')      
+		        {               
+		                mas[j][i-pos] = '\0';     
+		                pos=i+1;           
+		                str[j]=mas[j];   
+		                j++;            
+		                if(j>=99)  break ;
+			}            
+			else            
+			{              
+				mas[j][i-pos] = str[i];  
+			};
+				
+		};
 		
-		switch(pid = fork()) {
+		printf(" %c %c %c %c ", mas[0][0], mas[0][1], mas[1][0], mas[1][1]);
+				
+		switch(pid = fork()) 
+		{
 			case 0:
-				execvp(str, argv);
+				execvp(mas, argv); 
 			default: 
 				wait(&status);
 				printf("return code: %d", WEXITSTATUS(status));
 		}
-		printf("\n");
-	}
 
-	
+		printf("\n");
+	};
+
 	return 0;
 
 }
+
