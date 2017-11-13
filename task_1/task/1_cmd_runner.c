30b9c76c3826357978cd2d370df24a36541e86c5
SonyaStroganova <you@example.com>
2017-11-13 16:18:52 +0300

    new

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -22,50 +22,56 @@ int main(int atgc, char *argv[], char *envp[])
 {	
 	pid_t pid;
 	int status;
-	char *arg[100];   
-	char mas[100][100] = {""};
+	char *mas[100];
 	char str[1000];
-	int len, pos, i=0, j = 0;
+	int len, k=0, i=0, j = 0, pos;
 	
+	for(int f = 0; f < 100; f++) 
+		mas[f] = new char[100];
 
 	while(1) 
 	{
 		gets(str);
 		len = strlen(str);
+		pos=0;
+		j = 0;
 		
 		for(i=0; i<len; i++)
 		{	
-
 			if (str[i] == ' '|| str[i] == '\0' || str[i] == '\n')      
-		        {               
-		                mas[j][i-pos] = '\0';     
-		                pos=i+1;           
-		                str[j]=mas[j];   
-		                j++;            
-		                if(j>=99)  break ;
+		        {             
+				pos = 0;	
+				j++;
 			}            
 			else            
 			{              
-				mas[j][i-pos] = str[i];  
+			  mas[j][pos] = str[i];
+		          pos++;
 			};
 				
 		};
 		
-		printf(" %c %c %c %c ", mas[0][0], mas[0][1], mas[1][0], mas[1][1]);
+		for(int ss = 0; ss <= j; ss++)
+			printf("\"%s\",", mas[ss]);
+
+		mas[j+1] = NULL;
+		
 				
 		switch(pid = fork()) 
 		{
 			case 0:
-				execvp(mas, argv); 
+				execvp(mas[0], mas); 
 			default: 
 				wait(&status);
 				printf("return code: %d", WEXITSTATUS(status));
 		}
 
+		
 		printf("\n");
 	};
 
 	return 0;
 
 }
+
 
